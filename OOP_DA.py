93fd82ede37931261aaacb84f20d20f8a898439f
Test <ksyuzlo7073@gmail.com>
2024-12-05 18:41:41 +1000

    Code and readme added

--- OOP_DA.py
+++ OOP_DA.py
@@ -1,1 +1,43 @@
+class Student:
+    def __init__(self, name, last_name, gender):
+        self.name = name
+        self.last_name = last_name
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+        
+    def add_finished_courses(self, course):
+        self.finished_courses.append(course)
+        if course in self.courses_in_progress:
+            self.courses_in_progress.remove(course)
+        
+class Mentor:
+    def __init__(self, name, last_name):
+        self.name = name
+        self.last_name = last_name
+        self.courses_attached = []
+        
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Îøèáêà'
+        
+class Lector(Mentor):
+    def __init__(self):
+        pass
+    
+class Reviewer(Mentor):
+    def __init__(self):
+        pass    
+    
+        
+lector = Lector('Vitya', 'Barashkin')
+print(lector.__dict__)
+
+
 
